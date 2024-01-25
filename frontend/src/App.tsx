0f62d2c6573ce50f3b0d2e2ebb2fb6e82937095c
Kadunapikin <ibakoshi27@gmail.com>
2024-01-25 21:52:14 +0100

    add frontend dir and set up dev environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-joy'>
+        <h1>Hello Joy, are you sure you bath this evening?</h1>
+      </div>
+      <div className='btn'>
+        <button>Yes</button> <span>or</span> <button>No</button>
+      </div>
+    </>
+  )
+}
+
+export default App
