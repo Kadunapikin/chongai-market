34fa1fc69be9af790f690f5b57e7225b12f348b8
Kadunapikin <ibakoshi27@gmail.com>
2024-01-30 00:58:16 +0100

    add SignUp.tsx component

---
+++ frontend/src/components/authentication/SignUp.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const SignUp: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Handle login logic here
+        console.log(email, password);
+    };
+
+    return (
+        <div className="container mx-auto p-4">
+            <form onSubmit={handleSubmit}>
+            <div>
+                    <label>Username:</label>
+                    <input
+                        type="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">SignUp</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
