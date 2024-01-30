16ce5e758950de05a5d4fabf869408993ec90eeb
Kadunapikin <ibakoshi27@gmail.com>
2024-01-30 01:44:26 +0100

    update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
     return (
 <header className="bg-blue-600 text-white p-4 shadow-md">
     <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">My E-Commerce Site</h1>
-        <nav>
-            <a href="/" className="inline-block p-2 hover:bg-blue-700 rounded">Home</a>
+        <nav className='flex'>
+            {/* <a href="/" className="inline-block p-2 hover:bg-blue-700 rounded">Home</a>
             <a href="/cart" className="inline-block p-2 hover:bg-blue-700 rounded">Cart</a>
-            <a href="/login" className="inline-block p-2 hover:bg-blue-700 rounded">Login</a>
+            <a href="/login" className="inline-block p-2 hover:bg-blue-700 rounded">Login</a> */}
+            <div className="inline-block p-2 hover:bg-blue-700 rounded">
+                <Link to="/">Home</Link>
+            </div>
+            <div className="inline-block p-2 hover:bg-blue-700 rounded">
+                <Link to="/cart">Cart</Link>
+            </div>
+            <div className="inline-block p-2 hover:bg-blue-700 rounded">
+                <Link to="/login">Login</Link>
+            </div>
             // Add more navigation links as needed
         </nav>
     </div>
