eb01e36c2d68f7b6514a52e5ab83d48a9963b0bb
Kadunapikin <ibakoshi27@gmail.com>
2024-02-03 23:48:51 +0100

    modify main.tsx to provide Redux store to the App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Use the Provider component to pass the Redux store to React.
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
