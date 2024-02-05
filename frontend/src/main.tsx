abe19615e7d67756adccd8eabca5635736397196
Kadunapikin <ibakoshi27@gmail.com>
2024-02-05 16:23:08 +0100

    modify main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,20 @@
-// src/index.tsx
+// src/main.tsx or src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+// Check if 'container' is not null to satisfy TypeScript's strict null checks
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    );
+}
